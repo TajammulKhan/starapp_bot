02ba4cda9094a5a83a52a67575410bc9bc619134
TajammulKhan <tajammul.khan@tibilsolutions.com>
2025-02-24 12:56:27 +0530

    updated port check

--- backend/index.js
+++ backend/index.js
@@ -102,7 +102,7 @@ app.post("/add-outcome", (req, res) => {
     }
 });
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
     console.log(`✅ StarApp Bot is running on port ${PORT}`);
 });
