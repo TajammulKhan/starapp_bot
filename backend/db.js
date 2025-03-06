12f57a52abd3677433d63dac421485a2dd79fded
TajammulKhan <tajammul.khan@tibilsolutions.com>
2025-03-06 15:48:38 +0530

    updated db name

--- backend/db.js
+++ backend/db.js
@@ -3,7 +3,7 @@ const { Pool } = require("pg");
 const pool = new Pool({
   user: process.env.DB_USER || "postgres",
   host: process.env.DB_HOST || "localhost",
-  database: process.env.DB_NAME || "starapp_new",
+  database: process.env.DB_NAME || "starapp_bot",
   password: process.env.DB_PASSWORD || "12345678",
   port: process.env.DB_PORT || 5432,
 });
