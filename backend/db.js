843fa2306079e4df9a32c055b36eca5940153579
TajammulKhan <tajammul.khan@tibilsolutions.com>
2025-03-06 16:13:12 +0530

    updated host name with ipv4

--- backend/db.js
+++ backend/db.js
@@ -2,7 +2,7 @@ const { Pool } = require("pg");
 
 const pool = new Pool({
   user: process.env.DB_USER || "postgres",
-  host: process.env.DB_HOST || "127.0.0.1",
+  host: process.env.DB_HOST || "192.168.1.108",
   database: process.env.DB_NAME || "starapp_bot",
   password: process.env.DB_PASSWORD || "12345678",
   port: process.env.DB_PORT || 5432,
