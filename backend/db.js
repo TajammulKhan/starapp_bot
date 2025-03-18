eb79873a9b47d406f35a4678982b2d41f23e921c
TajammulKhan <tajammul.khan@tibilsolutions.com>
2025-03-18 12:43:59 +0530

    added greeting msg via db check 2

--- backend/db.js
+++ backend/db.js
@@ -1,10 +1,11 @@
 const { Pool } = require("pg");
+require("dotenv").config(); // Load environment variables
 
 const pool = new Pool({
-  user: process.env.DB_USER || "postgres",
-  host: process.env.DB_HOST || "34.47.207.238",
-  database: process.env.DB_NAME || "starapp_new",
-  password: process.env.DB_PASSWORD || "starapp@123",
+  user: process.env.DB_USER,
+  host: process.env.DB_HOST,
+  database: process.env.DB_NAME,
+  password: process.env.DB_PASSWORD,
   port: process.env.DB_PORT || 5432,
 });
 
